8efb94bb559d9c1ab7addc7fbf1fea9bd252bef2
Gugan <110531967+kokkigugan@users.noreply.github.com>
2024-07-05 11:27:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,44 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer, dev, webpack }) => {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  }),
+  webpack: (config, { isServer, dev, webpack }) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false
+    };
+
+    config.externals.push(
+      'pino-pretty',
+      'lokijs',
+      'encoding'
+    );
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/events',
+        permanent: false
       }
-   
-      config.externals.push(
-        'pino-pretty',
-        'lokijs',
-        'encoding'
-      )
-   
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-      
-      return config
-    },
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/events',
-          permanent: false
-        }
-      ]
-    }
-  }
-   
-  export default nextConfig
+    ];
+  },
+  images: {
+    domains: ['avatars.azuro.org'],
+  },
+};
+
+export default nextConfig;
